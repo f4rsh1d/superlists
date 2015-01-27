d261030fba4259da08895be867111b6391e5ca16
Farshid <farshid.imanipour@gmail.com>
2015-01-27 23:39:32 +0330

    Refactored FT with a helper method.

--- functional_tests.py
+++ functional_tests.py
@@ -12,9 +12,15 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
-        # to chekc out its homepage
+        # to check out its homepage
         self.browser.get('http://localhost:8000')
 
         # She notices the homepage title and header mention to-do lists
@@ -29,13 +35,10 @@ class NewVisitorTest(unittest.TestCase):
         # She types "Buy peacock feathers" into a text box (Edith's hobby is tying fly-fishing lures)
         inputbox.send_keys('Buy peacock feathers')
 
-        # When shi hits enter, the page updates, and now the page lists
+        # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do lists
         inputbox.send_keys(Keys.ENTER)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        # self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows), "New to-do item did not appear in table -- its text was:\n%s" % (table.text,))
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly" (Edith is very methodical)
@@ -43,12 +46,9 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
-
         # The page updates again, and now shows both items on her lists
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # Edith wonders wheter the site will remember her list. Then she sees
         # that the sute has generated a unique URL for her -- there is some
