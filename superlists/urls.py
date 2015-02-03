74d496afe9c5ce8a05b7547fbcffde05278c6a7a
Farshid <farshid.imanipour@gmail.com>
2015-02-03 06:42:19 +0330

    spiked in custom auth backend with persona

--- superlists/urls.py
+++ superlists/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls import patterns, url, include
 urlpatterns = patterns('',
                        url(r'^$', 'lists.views.home_page', name='home'),
                        url(r'^lists/', include('lists.urls')),
+                       url(r'^accounts/', include('accounts.urls')),
                        # url(r'^admin/', include(admin.site.urls)),
                        )
