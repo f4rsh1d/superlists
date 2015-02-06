c4ba89e931f7624dc83962e75167349824a30d66
Farshid <farshid.imanipour@gmail.com>
2015-02-06 08:27:00 +0330

    login now works correctly

--- lists/migrations/0001_initial.py
+++ lists/migrations/0001_initial.py
@@ -13,10 +13,31 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Item',
             fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
+                ('text', models.TextField(default='')),
+            ],
+            options={
+                'ordering': ('id',),
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='List',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
             ],
             options={
             },
             bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='item',
+            name='list',
+            field=models.ForeignKey(to='lists.List', default=None),
+            preserve_default=True,
+        ),
+        migrations.AlterUniqueTogether(
+            name='item',
+            unique_together=set([('list', 'text')]),
         ),
     ]
