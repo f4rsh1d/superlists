04ca0ebc945cb9f98b4e5ad58dafb24c53b5db68
Farshid <farshid.imanipour@gmail.com>
2015-02-07 17:22:11 +0330

    url, placeholder view, and first-cut templates for my_lists

--- accounts/authentication.py
+++ accounts/authentication.py
@@ -1,8 +1,9 @@
 import requests
+import logging
 from django.contrib.auth import get_user_model
 from django.conf import settings
 User = get_user_model()
-
+logger = logging.getLogger(__name__)
 PERSONA_VERIFY_URL = 'https://verifier.login.persona.org/verify'
 
 
@@ -19,6 +20,10 @@ class PersonaAuthenticationBackend(object):
                 return User.objects.get(email=email)
             except User.DoesNotExist:
                 return User.objects.create(email=email)
+        else:
+            logger.warning(
+                'Persona says no. Json was: {}'.format(response.json())
+            )
 
     def get_user(self, email):
         try:
